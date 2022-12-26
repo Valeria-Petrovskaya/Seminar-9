43f1a0f63ddf29a1c4ded9e48c0271b790342a20
Lera <miss_val@bk.ru>
2022-12-26 09:11:11 +0300

    Задача 66

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿int rec (int M, int N)
+{
+    if (M > N)
+    return N;
+    return ((M + N) * (N - M + 1))/2;
+}
+Console.Clear();
+Console.Write("Введите M и N: ");
+int [] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(rec(numbers[0], numbers [1]));
