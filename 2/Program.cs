80fd40484f24c8e0fec415e2b84e1712697e99e0
Lera <miss_val@bk.ru>
2022-12-26 09:11:49 +0300

    Задача 68

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результат: {A(m, n)}");
